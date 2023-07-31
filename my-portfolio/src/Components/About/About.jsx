1e7d8e3e1c95b786d063dc7beaacdf1000e6d0b6
krishnavasu <76085113+krishnavasu@users.noreply.github.com>
2023-07-31 08:26:51 +0530

    Add files via upload

---
+++ my-portfolio/src/Components/About/About.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import "./About.css";
+import { Type } from "./Type";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+import PhoneIcon from "@material-ui/icons/Phone";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import DescriptionIcon from "@material-ui/icons/Description";
+// import PublicIcon from "@material-ui/icons/Public";
+import { Introduction } from "./Introduction";
+import { Timeline } from "./Timeline";
+import { Techstacks } from "./Techstacks";
+// import Button from "@mui/material/Button";
+export const About = () => {
+  return (
+    <>
+      <div className="about center">
+        <h1 data-aos="fade-right" className="mobileHead">
+          Hello, I am <span className="about__name">Shreyas Khakal</span>
+        </h1>
+        <Type />
+        <p className="about__desc" data-aos="fade-right">
+          Passionate and creative Full Stack Developer having proficiency in
+          React js. Attired with a variety of tools & technologies and keen to
+          learn new one.
+        </p>
+        <div className="about__contact center">
+          <a
+            href="https://github.com/krishnavasu/AlgoBulls"
+            aria-label="github"
+            target="_blank"
+            rel="noreferrer"
+            className="link link--icon"
+          >
+            <GitHubIcon />
+          </a>
+          <a
+            href="mailto:gopalsachan7@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="mail"
+            className="link link--icon"
+          >
+            <EmailIcon />
+          </a>
+          <a
+            href="tel:+916392999575"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="phone"
+            className="link link--icon"
+          >
+            <PhoneIcon />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/gopalsachan0"
+            aria-label="linkedin"
+            className="link link--icon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LinkedInIcon />
+          </a>
+
+        </div>
+
+        <button
+          className="btnResume"
+          onClick={() => {
+            window.open(
+              "https://drive.google.com/file/d/1cZx3lKMjRe0zG7Jan0KZKLB-hs-UbqA3/view?usp=drivesdk"
+            );
+          }}
+        >
+          Resume
+        </button>
+      </div>
+      <Introduction />
+      <Timeline />
+      <section id="#skills">
+        <Techstacks />
+      </section>
+    </>
+  );
+};
---
+++ my-portfolio/src/Components/About/Timeline.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import "./Timeline.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+import WorkIcon from "@material-ui/icons/Work";
+import SchoolIcon from "@material-ui/icons/School";
+import StarRateIcon from "@material-ui/icons/StarRate";
+import { ThemeContext } from "../../Context/theme";
+
+export const Timeline = () => {
+  const [{ themename }] = React.useContext(ThemeContext);
+  const [linecolor, setlinecolor] = React.useState(
+    themename === "light" ? "#23283e" : "#fcfcfc"
+  );
+
+  React.useEffect(() => {
+    if (themename === "dark") {
+      setlinecolor("#fcfcfc");
+    } else {
+      setlinecolor("#23283e");
+    }
+  }, [themename]);
+  return (
+    <>
+      <div className="section mainsection">
+        <h2 className="section__title" data-aos="fade-right">
+          My <span className="different">Timeline 💫</span>
+        </h2>
+        <VerticalTimeline lineColor={linecolor}>
+          <VerticalTimelineElement
+            date={"Mar 2023 - May 2023"}
+            contentStyle={{
+              boxShadow: `var(--shadow)`,
+              border: "3px solid var(--clr-primary)",
+              backgroundColor: `var(--clr-bg)`,
+              textAlign: "center",
+              color: `var(--clr-fg-alt)`,
+            }}
+            contentArrowStyle={{
+              borderRight: "16px solid  var(--clr-primary)",
+            }}
+            iconStyle={{
+              border: ` 3px solid var(--clr-primary)`,
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-primary)`,
+            }}
+            icon={<WorkIcon />}
+          >
+            <h3
+              className="vertical-timeline-element-title"
+              data-aos="fade-right"
+            >
+              Intern
+            </h3>
+            <h4
+              className="vertical-timeline-element-subtitle"
+              data-aos="fade-right"
+            >
+              MKT softwares
+            </h4>
+            <p data-aos="fade-right">
+              Work as react Developer.
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            date={"Sep 2019 - Oct 19"}
+            contentStyle={{
+              boxShadow: `var(--shadow)`,
+              border: "3px solid var(--clr-primary)",
+              backgroundColor: `var(--clr-bg)`,
+              textAlign: "center",
+              color: `var(--clr-fg-alt)`,
+            }}
+            contentArrowStyle={{
+              borderRight: "16px solid  var(--clr-primary)",
+            }}
+            iconStyle={{
+              border: ` 3px solid var(--clr-primary)`,
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-primary)`,
+            }}
+            icon={<WorkIcon />}
+          >
+            <h3
+              className="vertical-timeline-element-title"
+              data-aos="fade-right"
+            >
+              Bootathon Virtual Lab
+            </h3>
+            <h4
+              className="vertical-timeline-element-subtitle"
+              data-aos="fade-right"
+            >
+              Masai School
+            </h4>
+            <p data-aos="fade-right">
+              Develop the UI of the lab in HTML and CSS.
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            icon={<StarRateIcon />}
+            iconStyle={{
+              border: ` 3px solid var(--clr-primary)`,
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-primary)`,
+            }}
+          ></VerticalTimelineElement>
+        </VerticalTimeline>
+      </div>
+    </>
+  );
+};
